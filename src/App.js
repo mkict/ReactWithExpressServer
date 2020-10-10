436b3aeaf986799c899a4c9e7e38087402d835e7
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-10-10 08:16:13 +0700

    change App to function component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from "react";
 
-class App extends React.Component {
-    render() {
-        return <div>
-        Hello
-        <h1>
-        	Hi555
-        </h1>
-        </div>;
-    }
-}
+const App = () => {
+  return (
+    <div>
+      Hello
+      <h1>Hi555</h1>
+    </div>
+  );
+};
 
 export default App;
